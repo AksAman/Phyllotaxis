cd3a948a1bda4a8adca1275fddca94b05b6f681e
AksAman <AksAman@github.com>
2018-06-29 21:28:24 +0530

    Edited camera movement
    Now camera stays at a fixed offet to the TrailRenderer component.

--- Assets/CamMove.cs
+++ Assets/CamMove.cs
@@ -8,10 +8,14 @@ public class CamMove : MonoBehaviour {
 	public float angle;
 	// Use this for initialization
 	public Visualizer visual;
+
+	public Transform trailGO;
+	public float offset;
 	
 	// Update is called once per frame
 	void Update () {
-		transform.Translate (Vector3.forward * speed * Time.deltaTime * visual.audioBandBuffer[0]*100);
+//		transform.Translate (Vector3.forward * speed * Time.deltaTime * visual.audioBandBuffer[0]*100);
+		this.transform.position = new Vector3 (0, 0, trailGO.position.z - offset);
 		transform.RotateAround (Vector3.forward, angle*Mathf.Deg2Rad * visual.audioBandBuffer[0]*10);
 	}
 }
