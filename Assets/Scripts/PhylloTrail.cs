84ce6e986d7d2a8bf9235793fd8b8d7cd43f0b8f
AksAman <AksAman@github.com>
2018-06-30 00:06:40 +0530

    Added scaling on audio!!
    Yayy

--- Assets/Scripts/PhylloTrail.cs
+++ Assets/Scripts/PhylloTrail.cs
@@ -6,43 +6,68 @@ public class PhylloTrail : MonoBehaviour {
 
 	// Basic Variables
 	private TrailRenderer trailRenderer;
+
+	[Tooltip("Angle and Scale")]
 	public float angle, scale;
 	public int startNumber;
 	int currentnumber;
+
 	public int stepSize;
 	private Vector2 phylloPosition;
+	public int zOffset;
+	public float zSmallOffset;
 
 	// Iterations
 	public int maxIterations;
 	private int currentIteration;
-	public float percentageComplete;
+
+	[Header("Audio Visualizer")]
+	public Visualizer visual;
 
 	// Lerping
+	[Header("Lerping Options")]
 	public bool useLerping;
 	private bool isLerping;
 	private Vector3 startPos, endPos;
 	public AnimationCurve lerpSpeedCurve;
 	public Vector2 lerpPosMinMaxSpeed;
-	public float lerpPosTimer, lerpPosSpeed;
+	private float lerpPosTimer, lerpPosSpeed;
 	public int lerpBand;
 
 	// Audio
-	public Visualizer visual;
 	public Color trailColor;
 	private Material trailMat;
 
+	// Repeat?
+	public bool repeat, invert;
+	private bool forward;
 
+	//Scaling on audio
+	[Header("Scaling on audio Options")]
+	public bool useScaling;
+	public bool useScaleAnimCurve;
+	public Vector2 scaleMinMax;
+	public int scaleBand;
+	public AnimationCurve scaleAnimCurve;
+	public float scaleTimer;
+	public float currentScale, scaleSpeed;
+
+	[Header("3D Trails?")]
 	public bool moveIn3D;
 
 	void Awake()
 	{
+		currentScale = scale;
+		zOffset = 0;
 		trailRenderer = GetComponent<TrailRenderer> ();
 //		trailMat = new Material (trailRenderer.sharedMaterial);
 //		trailMat.SetColor ("_TintColor", trailColor);
 //		trailRenderer.sharedMaterial = trailMat;
 
+		forward = true;
+
 		currentnumber = startNumber;
-		transform.localPosition = CalculatePosition (angle, currentnumber, scale);
+		transform.localPosition = CalculatePosition (angle, currentnumber, currentScale);
 		if (useLerping)
 		{
 			isLerping = true;
@@ -52,70 +77,89 @@ public class PhylloTrail : MonoBehaviour {
 
 	void Update()
 	{
+		if(useScaling)
+		{
+			if(useScaleAnimCurve)
+			{
+				scaleTimer = visual.audioBandBuffer [scaleBand] * Time.deltaTime * scaleSpeed;
+				currentScale = Mathf.Lerp (scaleMinMax.x, scaleMinMax.y, scaleAnimCurve.Evaluate(scaleTimer));
+
+				if(scaleTimer > 1)
+				{
+					scaleTimer -= 1;
+				}
+			}
+			else if (!useScaleAnimCurve) 
+			{
+				currentScale = Mathf.Lerp (scaleMinMax.x, scaleMinMax.y, visual.audioBandBuffer [scaleBand] * scaleSpeed);
+			}
+		}
+
 		if(useLerping)
 		{
 			if(isLerping)
 			{
 				float audioValue = visual.audioBandBuffer [lerpBand] * 1000;
 				audioValue = Mathf.Clamp01(audioValue);
-				Debug.Log ("On Anim curve : " + lerpSpeedCurve.Evaluate (audioValue));
 				lerpPosSpeed = Mathf.Lerp (lerpPosMinMaxSpeed.x, lerpPosMinMaxSpeed.y, lerpSpeedCurve.Evaluate (audioValue));
 				lerpPosTimer += Time.deltaTime * lerpPosSpeed;
 
 				transform.localPosition = Vector3.Lerp (startPos, endPos, Mathf.Clamp01(lerpPosTimer));
-				Debug.Log (lerpPosTimer);
 				if(lerpPosTimer >= 1)
 				{
 					lerpPosTimer -= 1;
-					currentnumber += stepSize;
-					currentIteration++;
-					StartLerping ();
+					if(forward)
+					{
+						currentnumber += stepSize;
+						currentIteration++;
+						StartLerping ();
+					}
+					else if(!forward)
+					{
+						currentnumber -= stepSize;
+						currentIteration--;
+
+					}
+
+					if(currentIteration > 0 && currentIteration < maxIterations)
+					{
+						StartLerping ();
+					}
+
+					else
+					{
+						if(repeat)
+						{
+							if(invert)
+							{
+								forward = !forward;
+								StartLerping ();
+							}
+
+							else
+							{
+								currentIteration = 0;
+								currentnumber = startNumber;
+								StartLerping ();
+							}
+						}
+
+						else if(!repeat)
+						{
+							isLerping = false;
+						}
+					}
 				}
 			}
 		}
 
 		if(!useLerping)
 		{
-			transform.localPosition = CalculatePosition (angle, currentnumber, scale);
+			transform.localPosition = CalculatePosition (angle, currentnumber, currentScale);
 			currentnumber += stepSize;
 			currentIteration++;
 		}
 	}
-
-	/* void FixedUpdate()
-	{
-		if (useLerping) 
-		{
-			if(isLerping)
-			{
-				timeSinceLerping = Time.time - timeStartedlerping;
-				percentageComplete = timeSinceLerping / lerpInterval;
-				transform.localPosition = Vector3.Lerp (startPos, endPos, percentageComplete);
-
-				if(percentageComplete >=0.97f)
-				{
-					transform.localPosition = endPos;
-					isLerping = false;
-					currentnumber += stepSize;
-					currentIteration++;
-					if(currentIteration <= maxIterations)
-					{
-						StartLerping();
-					}
-					else
-					{
-						isLerping = false;
-					}
-				}
-			}
-		}
-
-		else
-		{
-			transform.localPosition = CalculatePosition (angle, currentnumber, scale);
-			currentnumber += stepSize;
-		}
-	}*/
 
 	Vector2 CalculatePosition(float _angle, int _number, float _scale)
 	{
@@ -128,9 +172,10 @@ public class PhylloTrail : MonoBehaviour {
 
 	void StartLerping()
 	{
+		zOffset += 1;
 		startPos = transform.position;
-		phylloPosition = CalculatePosition (angle, currentnumber, scale);
-		float _z = moveIn3D ? currentnumber : 0;
+		phylloPosition = CalculatePosition (angle, currentnumber, currentScale);
+		float _z = moveIn3D ? zOffset * zSmallOffset: 0;
 		endPos = new Vector3 (phylloPosition.x, phylloPosition.y, _z);
 	}
 }
